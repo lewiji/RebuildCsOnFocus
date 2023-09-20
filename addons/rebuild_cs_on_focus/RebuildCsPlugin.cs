617536942c0ce2a79d00b4bc8db3aa488384c209
lewiji <lewiji@gmail.com>
2023-09-20 09:03:09 +0100

    Alternative method of triggering build that should work across 4.1 & 4.2

--- addons/rebuild_cs_on_focus/RebuildCsPlugin.cs
+++ addons/rebuild_cs_on_focus/RebuildCsPlugin.cs
@@ -10,90 +10,88 @@ namespace RebuildCsOnFocus.addons.rebuild_cs_on_focus;
 [Tool]
 public partial class RebuildCsPlugin : EditorPlugin
 {
-	RebuildOnFocusUi _rebuildOnFocusUi = default!;
-	Callable _buildCallable;
-	bool _enabled;
-	bool _scanning;
-	public override void _EnterTree()
-	{
-		AddUiControl();
-		FindEditorBuildShortcut();
-		ConnectEditorSignals();
-	}
-	
-	public override void _ExitTree()
-	{
-		_enabled = false;
-		_scanning = false;
-		RemoveControlFromContainer(CustomControlContainer.Toolbar, _rebuildOnFocusUi);
-		_rebuildOnFocusUi.QueueFree();
-	}
+   RebuildOnFocusUi _rebuildOnFocusUi = default!;
+   Callable _buildCallable;
+   bool _enabled;
+   bool _scanning;
 
-	void ConnectEditorSignals()
-	{
-		GetTree().Root.Connect(Window.SignalName.FocusEntered, new Callable(this, MethodName.RootOnFocusEntered));
-		GetEditorInterface().GetResourceFilesystem().Connect(EditorFileSystem.SignalName.ResourcesReload, new Callable(this, MethodName.OnResourcesReload));
-	}
+   public override void _EnterTree()
+   {
+      AddUiControl();
+      FindEditorBuildShortcut();
+      ConnectEditorSignals();
+   }
 
-	void AddUiControl()
-	{
-		var dir = GetScript().As<CSharpScript>().ResourcePath.GetBaseDir();
-		_rebuildOnFocusUi = GD.Load<PackedScene>($"{dir}/rebuild_on_focus.tscn").Instantiate<RebuildOnFocusUi>();
-		_rebuildOnFocusUi.Connect(RebuildOnFocusUi.SignalName.Enabled, new Callable(this, MethodName.RebuildOnFocusUiOnEnabled));
-		_rebuildOnFocusUi.Connect(RebuildOnFocusUi.SignalName.Disabled, new Callable(this, MethodName.RebuildOnFocusUiOnDisabled));
-		AddControlToContainer(CustomControlContainer.Toolbar, _rebuildOnFocusUi);
-	}
+   public override void _ExitTree()
+   {
+      _enabled = false;
+      _scanning = false;
+      RemoveControlFromContainer(CustomControlContainer.Toolbar, _rebuildOnFocusUi);
+      _rebuildOnFocusUi.QueueFree();
+   }
 
-	void OnResourcesReload(string[] resources)
-	{
-		if (_scanning && resources.Any(res => res.GetFile().GetExtension() == "cs"))
-		{
-			_buildCallable.Call();
-		}
-	}
+   void ConnectEditorSignals()
+   {
+      GetTree().Root.Connect(Window.SignalName.FocusEntered, new Callable(this, MethodName.RootOnFocusEntered));
 
-	void RootOnFocusEntered()
-	{
-		if (!_enabled) return;
-		_scanning = true;
-		GetEditorInterface().GetResourceFilesystem().ScanSources();
-	}
+      GetEditorInterface()
+         .GetResourceFilesystem()
+         .Connect(EditorFileSystem.SignalName.ResourcesReload, new Callable(this, MethodName.OnResourcesReload));
+   }
 
-	public override void _Process(double delta)
-	{
-		if (!_scanning) return;
-		if (GetEditorInterface().GetResourceFilesystem().IsScanning()) return;
-		_scanning = false;
-	}
+   void AddUiControl()
+   {
+      var dir = GetScript().As<CSharpScript>().ResourcePath.GetBaseDir();
+      _rebuildOnFocusUi = GD.Load<PackedScene>($"{dir}/rebuild_on_focus.tscn").Instantiate<RebuildOnFocusUi>();
+      _rebuildOnFocusUi.Connect(RebuildOnFocusUi.SignalName.Enabled, new Callable(this, MethodName.RebuildOnFocusUiOnEnabled));
+      _rebuildOnFocusUi.Connect(RebuildOnFocusUi.SignalName.Disabled, new Callable(this, MethodName.RebuildOnFocusUiOnDisabled));
+      AddControlToContainer(CustomControlContainer.Toolbar, _rebuildOnFocusUi);
+   }
 
-	void RebuildOnFocusUiOnEnabled()
-	{
-		_enabled = true;
-	}
-	
-	void RebuildOnFocusUiOnDisabled()
-	{
-		_enabled = false;
-	}
-    
-	// Hacky way to fetch build button shortcut, since API access to build system or editor shortcuts isn't exposed
-	void FindEditorBuildShortcut()
-	{
-		var toolbarNodes = _rebuildOnFocusUi.GetParent().GetChildren();
-		foreach (var toolbarNode in toolbarNodes)
-		{
-			if (toolbarNode is not Button {Visible: true} button) continue;
-			
-			var signalConnectionList = button.GetSignalConnectionList(BaseButton.SignalName.Pressed);
+   void OnResourcesReload(string[] resources)
+   {
+      if (_scanning && resources.Any(res => res.GetFile().GetExtension() == "cs"))
+      {
+         _buildCallable.Call();
+      }
+   }
 
-			foreach (var dictionary in signalConnectionList)
-			{
-				var connection = dictionary["callable"].AsCallable();
-				if (connection.Delegate.Method.Name != "BuildProjectPressed") continue;
-				_buildCallable = connection;
-				break;
-			}
-		}
-	}
+   void RootOnFocusEntered()
+   {
+      if (!_enabled) return;
+      _scanning = true;
+      GetEditorInterface().GetResourceFilesystem().ScanSources();
+   }
+
+   public override void _Process(double delta)
+   {
+      if (!_scanning) return;
+      if (GetEditorInterface().GetResourceFilesystem().IsScanning()) return;
+      _scanning = false;
+   }
+
+   void RebuildOnFocusUiOnEnabled()
+   {
+      _enabled = true;
+   }
+
+   void RebuildOnFocusUiOnDisabled()
+   {
+      _enabled = false;
+   }
+
+   // Hacky way to fetch build button shortcut, since API access to build system or editor shortcuts isn't exposed
+   void FindEditorBuildShortcut()
+   {
+      var node = new Control();
+      AddControlToBottomPanel(node, "");
+      var bottomBar = node.GetParent();
+      RemoveControlFromBottomPanel(node);
+      node.QueueFree();
+
+      var msBuildPanel = bottomBar.GetChildren()
+         .First(c => c is VBoxContainer && c.HasMethod("BuildProject"));
+      _buildCallable = new Callable(msBuildPanel, "BuildProject");
+   }
 }
 #endif
